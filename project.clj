effa72ee176f40bd62e1735d6911013778bae970
Yogthos <dmitri.sotnikov@gmail.com>
2016-02-27 17:17:12 -0500

    changed to not use local state

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject luminus-jetty "0.1.0"
+(defproject luminus-jetty "0.1.1"
   :description "Jetty adapter for Luminus"
   :url "https://github.com/luminus-framework/luminus-jetty"
   :license {:name "Eclipse Public License"
--- src/luminus/http_server.clj
+++ src/luminus/http_server.clj
@@ -3,27 +3,21 @@
             [clojure.set :refer [rename-keys]]
             [qbits.jet.server :refer [run-jetty]]))
 
-(defonce http-server (atom nil))
-
 (defn start [{:keys [init port] :as opts}]
-  (if @http-server
-    (log/error "HTTP server is already running!")
-    (try
-      (init)
-      (reset! http-server
-              (run-jetty
-               (merge
+  (try
+    (init)
+    (reset! http-server
+            (run-jetty
+              (merge
                 {:join? false}
                 (-> opts
                     (dissoc :init)
                     (rename-keys {:handler :ring-handler})))))
-      (log/info "server started on port" port)
-      (catch Throwable t
-        (log/error t (str "server failed to start on port: " port))))))
+    (log/info "server started on port" port)
+    (catch Throwable t
+      (log/error t (str "server failed to start on port: " port)))))
 
-(defn stop [destroy]
-  (when @http-server
-    (destroy)
-    (.stop @http-server)
-    (reset! http-server nil)
-    (log/info "HTTP server stopped")))
+(defn stop [http-server destroy]
+  (destroy)
+  (.stop http-server)
+  (log/info "HTTP server stopped"))
