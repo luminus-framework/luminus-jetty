e18d9a8fe534ea67906607288ab50186aa1374ad
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-05-10 14:39:08 -0400

    bumped up jet

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject luminus-jetty "0.1.1"
+(defproject luminus-jetty "0.1.2"
   :description "Jetty adapter for Luminus"
   :url "https://github.com/luminus-framework/luminus-jetty"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [cc.qbits/jet "0.6.6"]])
+                 [cc.qbits/jet "0.7.6"]])
