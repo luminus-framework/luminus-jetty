738107ecde2ddabfb403ea24fe67be513dd82d9c
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2017-12-23 23:37:37 -0500

    bumped up dependencies, added core.async

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject luminus-jetty "0.1.5"
+(defproject luminus-jetty "0.1.6"
   :description "Jetty adapter for Luminus"
   :url "https://github.com/luminus-framework/luminus-jetty"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/tools.logging "0.4.0"]
+                 [org.clojure/core.async "0.3.465"]
                  [cc.qbits/jet "0.7.11"]])
