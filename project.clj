99c403ec80b2c51018c490b322226085e8415cbd
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2020-02-06 20:58:25 -0500

    updated dependencies

--- project.clj
+++ project.clj
@@ -3,9 +3,10 @@
   :url "https://github.com/luminus-framework/luminus-jetty"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/tools.logging "0.4.0"]
                  [org.clojure/core.async "0.3.465"]
                  [ring/ring-jetty-adapter "1.7.1"]
+                 [org.eclipse.jetty/jetty-server "9.4.26.v20200117"]
                  [org.eclipse.jetty.websocket/websocket-server "9.4.26.v20200117"]
                  [org.eclipse.jetty.websocket/websocket-servlet "9.4.26.v20200117"]])
