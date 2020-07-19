cb4ff24cf96b3221f4c1bc6fd99e36c52fc259e3
Marek Lipert <marek@spinney.io>
2020-07-19 12:52:46 +1200

    Update dependencies.

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/tools.logging "0.6.0"]
-                 [ring/ring-jetty-adapter "1.8.0"]
-                 [org.eclipse.jetty/jetty-server "9.4.26.v20200117"]
-                 [org.eclipse.jetty.websocket/websocket-server "9.4.26.v20200117"]
-                 [org.eclipse.jetty.websocket/websocket-servlet "9.4.26.v20200117"]])
+                 [org.clojure/tools.logging "1.1.0"]
+                 [ring/ring-jetty-adapter "1.8.1"]
+                 [org.eclipse.jetty/jetty-server "9.4.30.v20200611"]
+                 [org.eclipse.jetty.websocket/websocket-server "9.4.30.v20200611"]
+                 [org.eclipse.jetty.websocket/websocket-servlet "9.4.30.v20200611"]])
