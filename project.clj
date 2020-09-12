8cd3ee570af4babfc9baf850b6e24f01ef1c97ee
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2020-09-11 23:38:50 -0400

    bumped up Jetty

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject luminus-jetty "0.2.0"
+(defproject luminus-jetty "0.2.1"
   :description "Jetty adapter for Luminus"
   :url "https://github.com/luminus-framework/luminus-jetty"
   :license {:name "Eclipse Public License"
@@ -6,6 +6,6 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/tools.logging "1.1.0"]
                  [ring/ring-jetty-adapter "1.8.1"]
-                 [org.eclipse.jetty/jetty-server "9.4.30.v20200611"]
-                 [org.eclipse.jetty.websocket/websocket-server "9.4.30.v20200611"]
-                 [org.eclipse.jetty.websocket/websocket-servlet "9.4.30.v20200611"]])
+                 [org.eclipse.jetty/jetty-server "9.4.31.v20200723"]
+                 [org.eclipse.jetty.websocket/websocket-server "9.4.31.v20200723"]
+                 [org.eclipse.jetty.websocket/websocket-servlet "9.4.31.v20200723"]])
