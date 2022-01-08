cebb3d6f7abda8e994062154333509abe4b501ed
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2022-01-08 08:59:23 -0500

    bumped up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject luminus-jetty "0.2.2"
+(defproject luminus-jetty "0.2.3"
   :description "Jetty adapter for Luminus"
   :url "https://github.com/luminus-framework/luminus-jetty"
   :license {:name "Eclipse Public License"
